cb2c376ae1991d6cc35da3fc11ca2d9d486c7ace
mikeplayshsr <qihangzhao267@gmail.com>
2026-03-17 14:44:21 -0400

    Create qihangteleop.java

---
+++ qihangteleop.java
@@ -0,0 +1,32 @@
+public class qihangteleop extends LinearOpMode{
+   @Override
+public void runOpMode(){
+      QihangDrivetrain QihangDrivetrain = new QihangDrivetrain(hardwareMap);
+   waitForStart();
+   // Put run blocks here
+
+while (opModeIsActive()) {
+   // Put loop blocks here
+      /*if(gamepad1.a){
+      }
+      if(gamepad1.x){
+      }
+      */
+      QihangDrivetrain.setMotorPower(-gamepad1.right_stick_y, gamepad1.right_stick_x,gamepad1.left_stick_x);
+   if(gamepad1.a){
+      intake.setpower(1);
+   }
+   else{
+      intake.setpower(0);
+      }
+   if(gamepad.x){
+      lbrake.setpower(0.4);
+      rbrake.setpower(0.3);
+   }else{
+      lbrake.setpower(0.6);
+      rbrake.setpower(0.5);
+   }
+   }
+
+   }
+}
